f7a60e62a2fd3c085ebede5677e39b3f72f2c1d7
amit5669 <117837290+amit5669@users.noreply.github.com>
2025-03-02 15:58:14 +0530

    CLOUD

--- prac9.py
+++ prac9.py
@@ -1,1 +1,10 @@
-print("hello duniya")
+sudo apt install qemu-kvm libvirt-bin bridge-utils virtinst virt-manager
+
+sudo rm /var/lib/dpkg/lock
+
+
+root@ubuntu:/home/ubuntu# sudo dpkg -configure -a
+
+
+
+sudo apt-get instal qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virtinst virt-manager
